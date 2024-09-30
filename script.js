b51c1df398e600517d185c75ca2069fce9a73bdf
Carol <anacarolinasf0101@gmail.com>
2024-09-29 23:35:59 -0300

    Update changing pages

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-//Carousel
 const carousel = document.getElementById('carousel-div-js');
 const leftArrow = document.getElementById('left-arrow');
 const rightArrow = document.getElementById('right-arrow');
+
+const homePage = document.getElementById('home');
+const catalogoPage = document.getElementById('catalogo');
+const sobrePage = document.getElementById('sobre');
+const homeLink = document.querySelector('a[href="#home"]');
+const catalogoLink = document.querySelector('a[href="#catalogo"]');
+const sobreLink = document.querySelector('a[href="#sobre"]');
+
+const homeSquare = document.getElementById('square-home');
+const catalogoSquare = document.getElementById('square-catalogo');
+const sobreSquare = document.getElementById('square-sobre');
+
 let scrollInterval;
 
+
+// CAROUSEL //
 function startScrolling(direction) {
   scrollInterval = setInterval(() => {carousel.scrollLeft += direction * 500;}, 800);
   leftArrow.style.display = 'block';
@@ -17,10 +30,10 @@ leftArrow.addEventListener('mouseover', () => startScrolling(-1));
 rightArrow.addEventListener('mouseover', () => startScrolling(1));
 leftArrow.addEventListener('mouseout', stopScrolling);
 rightArrow.addEventListener('mouseout', stopScrolling);
+// CAROUSEL //
 
 
-
-//Scroll to Top Button
+// Scroll to Top Button //
 let button = document.getElementById("scroll-top");
 
 window.onscroll = function() {scrollFunction()};
@@ -37,9 +50,43 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// Scroll to Top Button //
 
 
-// Disable right-click
-//document.addEventListener('contextmenu', event => {
-//event.preventDefault();
-//});
+// CHANGE PAGES //
+  homeLink.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    catalogoPage.style.display = "none";
+    sobrePage.style.display = "none";
+    homePage.style.display = "block";
+
+    catalogoSquare.style.display = "none";
+    sobreSquare.style.display = "none";
+    homeSquare.style.display = "inline";
+  })
+
+  catalogoLink.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    homePage.style.display = "none";
+    sobrePage.style.display = "none";
+    catalogoPage.style.display = "block";
+
+    homeSquare.style.display = "none";
+    sobreSquare.style.display = "none";
+    catalogoSquare.style.display = "inline";
+  })
+
+  sobreLink.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    homePage.style.display = "none";
+    catalogoPage.style.display = "none";
+    sobrePage.style.display = "block";
+
+    homeSquare.style.display = "none";
+    catalogoSquare.style.display = "none";
+    sobreSquare.style.display = "inline";
+  })
+// CHANGE PAGES //
