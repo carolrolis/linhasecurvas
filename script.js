b24a6c25b9118cb49ee85b869776d0520ebb23aa
Carol <anacarolinasf0101@gmail.com>
2024-10-04 12:45:07 -0300

    Update carousels functions

--- script.js
+++ script.js
@@ -1,53 +1,98 @@
-// Variáveis DOM do carrossel da Home Page
-const carousel = document.getElementById('carousel-div-js-home');
-const leftArrow = document.getElementById('left-arrow-home');
-const rightArrow = document.getElementById('right-arrow-home');
-
-// Variável referente ao intervalo do carrossel de imagens
 let scrollInterval;
+const homeCarousel = document.getElementById('home-carousel-div-js');
+const homeLeftArrow = document.getElementById('home-left-arrow');
+const homeRightArrow = document.getElementById('home-right-arrow');
 
-// Variável DOM do botão de voltar para cima
+
 let button = document.getElementById("scroll-top");
 
-// Variáveis referentes à mudança de páginas através da navbar, do botão principal e do botão de voltar nos itens do catálogo
+
 const homePage = document.getElementById('home');
 const catalogoPage = document.getElementById('catalogo');
 const catalogoIntroPage = document.getElementById('catalogo-intro');
 const sobrePage = document.getElementById('sobre');
+
 const welcomeButton = document.getElementById('welcome-button');
-const backButton = document.getElementById('voltar-catalogo');
+const backButtonCatalogo = document.getElementById('back-button-catalogo');
+
 const homeLink = document.querySelector('a[href="#home"]');
 const catalogoLink = document.querySelector('a[href="#catalogo"]');
 const sobreLink = document.querySelector('a[href="#sobre"]');
-const homeSquare = document.getElementById('square-home');
-const catalogoSquare = document.getElementById('square-catalogo');
-const sobreSquare = document.getElementById('square-sobre');
+const homeSquare = document.getElementById('nav-square-home');
+const catalogoSquare = document.getElementById('nav-square-catalogo');
+const sobreSquare = document.getElementById('nav-square-sobre');
+
 
 // Variáveis DOM - NodeList de todos os elementos HTML <a href="...-item"> e <section id="...-item">
 const itemsLinks = document.querySelectorAll('a[href$="-item"]');
 const itemsSections = document.querySelectorAll('section[id$="-item"]');
 
 
+const catalogoLeftArrow = document.querySelector('.catalogo-left-arrow');
+const catalogoRightArrow = document.querySelector('.catalogo-right-arrow');
 
-// HOME - CAROUSEL //
+let changeImageValue;
+let itemMainImg;
+
+const farmacinhaImgs =
+  [
+    'images/farmacinha/farmacinha1.jpg',
+    'images/farmacinha/farmacinha2.jpg',
+    'images/farmacinha/farmacinha3.jpg',
+    'images/farmacinha/farmacinha4.jpg',
+    'images/farmacinha/farmacinha5.jpg',
+    'images/farmacinha/farmacinha6.jpg',
+    'images/farmacinha/farmacinha7.jpg',
+    'images/farmacinha/farmacinha8.jpg',
+    'images/farmacinha/farmacinha9.jpg'
+  ]
+const organizadoresImgs =
+  [
+    'images/organizadores/organizador1.jpg',
+    'images/organizadores/organizador2.jpg',
+    'images/organizadores/organizador3.jpg',
+    'images/organizadores/organizador4.jpg'
+  ]
+const ninhosImgs =
+[
+  'images/ninhos/ninho1.jpg',
+  'images/ninhos/ninho2.jpg',
+  'images/ninhos/ninho3.jpg',
+  'images/ninhos/ninho4.jpg',
+  'images/ninhos/ninho5.jpg',
+  'images/ninhos/ninho6.jpg',
+  'images/ninhos/ninho7.jpg'
+]
+
+const imgArrays = {
+  farmacinha: farmacinhaImgs,
+  organizadores: organizadoresImgs,
+  ninhos: ninhosImgs
+};
+let currentArray = [];
+let imgIndex = 0;
+
+
+
+// ========== HOME - CAROUSEL ==========
+homeLeftArrow.addEventListener('mouseover', () => startScrolling(-1));
+homeRightArrow.addEventListener('mouseover', () => startScrolling(1));
+homeLeftArrow.addEventListener('mouseout', stopScrolling);
+homeRightArrow.addEventListener('mouseout', stopScrolling);
+
 function startScrolling(direction) {
-  scrollInterval = setInterval(() => { carousel.scrollLeft += direction * 500; }, 800);
-  leftArrow.style.display = "block";
+  scrollInterval = setInterval(() => { homeCarousel.scrollLeft += direction * 500; }, 800);
+  homeLeftArrow.style.display = "block";
 }
 
 function stopScrolling() {
   clearInterval(scrollInterval);
 }
-
-leftArrow.addEventListener('mouseover', () => startScrolling(-1));
-rightArrow.addEventListener('mouseover', () => startScrolling(1));
-leftArrow.addEventListener('mouseout', stopScrolling);
-rightArrow.addEventListener('mouseout', stopScrolling);
-// HOME - CAROUSEL //
+// ========== HOME - CAROUSEL ==========
 
 
 
-// SCROLL TO TOP BUTTON //
+// ========== SCROLL TO TOP BUTTON ==========
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
@@ -62,17 +107,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-// SCROLL TO TOP BUTTON //
+// ========== SCROLL TO TOP BUTTON ==========
 
 
 
-// CHANGE PAGES - NAVBAR AND MAIN BUTTON //
-homeLink.addEventListener("click", () => homeDisplay());
-catalogoLink.addEventListener("click", () => catalogoDisplay());
-welcomeButton.addEventListener("click", () => catalogoDisplay());
-sobreLink.addEventListener("click", () => sobreDisplay());
+// ========== CHANGE PAGES - NAVBAR, WELCOME BUTTON AND BACKBUTTON CATALOGO ==========
+homeLink.addEventListener('click', () => homeDisplay());
+catalogoLink.addEventListener('click', () => catalogoDisplay());
+welcomeButton.addEventListener('click', () => catalogoDisplay());
+backButtonCatalogo.addEventListener('click', () => backToCatalogo());
+sobreLink.addEventListener('click', () => sobreDisplay());
+
 
 function homeDisplay() {
+  topFunction();
   catalogoPage.style.display = "none";
   sobrePage.style.display = "none";
   homePage.style.display = "block";
@@ -83,6 +131,7 @@ function homeDisplay() {
 }
 
 function catalogoDisplay() {
+  topFunction();
   homePage.style.display = "none";
   sobrePage.style.display = "none";
   catalogoPage.style.display = "block";
@@ -92,13 +141,23 @@ function catalogoDisplay() {
   catalogoSquare.style.display = "inline";
 
   catalogoIntroPage.style.display = "block";
-  backButton.style.display = "none";
+  backButtonCatalogo.style.display = "none";
+  itemsSections.forEach(section => {
+    section.style.display = "none";
+  })
+}
+
+function backToCatalogo() {
+  topFunction();
+  catalogoIntroPage.style.display = "block";
+  backButtonCatalogo.style.display = "none";
   itemsSections.forEach(section => {
     section.style.display = "none";
   })
 }
 
 function sobreDisplay() {
+  topFunction();
   catalogoPage.style.display = "none";
   homePage.style.display = "none";
   sobrePage.style.display = "block";
@@ -107,15 +166,16 @@ function sobreDisplay() {
   homeSquare.style.display = "none";
   sobreSquare.style.display = "inline";
 }
-// CHANGE PAGES - NAVBAR AND BUTTON //
+// ========== CHANGE PAGES - NAVBAR, WELCOME BUTTON AND BACKBUTTON CATALOGO ==========
 
 
 
-// CHANGE PAGES - CATALOGO SECTIONS //
+// ========== CHANGE PAGES - CATALOGO ITEM SECTIONS ==========
 itemsLinks.forEach(link => {
   link.addEventListener('click', () => selectCatalogoItem(link));
-  console.log(link);
+  changeImageCarousel();
 });
+
 
 function selectCatalogoItem(link) {
   topFunction();
@@ -126,23 +186,46 @@ function selectCatalogoItem(link) {
 
   catalogoIntroPage.style.display = "none";
   selectedItem.style.display = "block";
-  backButton.style.display = "block";
+  backButtonCatalogo.style.display = "block";
 
   itemsSections.forEach(section => {
     if (section !== selectedItem) {
       section.style.display = "none";
     }
   });
+
+  changeImageValue = hrefNew.replace("-item", "");
+  itemMainImg = document.getElementById(`catalogo-img-${changeImageValue}`);
+  currentArray = imgArrays[changeImageValue];
+}
+// ========== CHANGE PAGES - CATALOGO ITEM SECTIONS ========== //
+
+
+
+// ========== CHANGE IMAGES - CATALOGO ITEM SECTIONS ========== //
+function changeImageCarousel() {
+  if (currentArray && itemMainImg) {
+    itemMainImg.setAttribute("src", currentArray[imgIndex]);
+  }
 }
 
-backButton.addEventListener("click", () => backToCatalogo());
-
-function backToCatalogo() {
-  topFunction();
-  catalogoIntroPage.style.display = "block";
-  backButton.style.display = "none";
-  itemsSections.forEach(section => {
-    section.style.display = "none";
-  })
+function nextImage() {
+  if (imgIndex < currentArray.length - 1) {
+    imgIndex++;
+    changeImageCarousel();
+  } else if (imgIndex == currentArray.length - 1) {
+    imgIndex = 0;
+    changeImageCarousel();
+  }
 }
-// CHANGE PAGES - CATALOGO SECTIONS //
+
+function prevImage() {
+  if (imgIndex > 0) {
+    imgIndex--;
+    changeImageCarousel();
+  } else if (imgIndex == 0) {
+    imgIndex = currentArray.length - 1;
+    changeImageCarousel();
+  }
+}
+// ========== CHANGE IMAGES - CATALOGO ITEM SECTIONS ========== //
